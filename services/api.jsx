1483030da9aa0cc8b033ad958ef93a576b7c8179
Debjoyy Sarkar <152073307+Debjoy26@users.noreply.github.com>
2025-09-08 00:35:43 +0530

    Update api.jsx

--- services/api.jsx
+++ services/api.jsx
@@ -1,5 +1,5 @@
 // services/api.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8000';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://iei-sc-rcciit.onrender.com/';
 
 export const api = {
   getEvents: () => fetch(`${API_BASE_URL}/api/events/`).then(res => res.json()),
