50b6614d1592851c7f9b7b038ed5aa4a26c5863b
Debjoyy Sarkar <152073307+Debjoy26@users.noreply.github.com>
2025-09-08 02:03:08 +0530

    Update JoinUsPage.jsx

--- pages/JoinUsPage.jsx
+++ pages/JoinUsPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://iei-sc-rcciit.onrender.com/';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://ieisc-rcciit.onrender.com';
 
 const JoinUsPage = () => {
   return (
